1d4bbaf3dc418876e6506b80806c8c216c0b4723
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-12-31 09:42:50 +0600

    done loading paralal data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { lusitana } from '../ui/fonts/fonts'
+import { Card } from '../ui/dashboard/cards'
+import RevenueChart from '../ui/dashboard/revenue-chart'
+import LatestInvoices from '../ui/dashboard/latest-invoices'
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data'
 
-export default function Page() {
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  // calculation All Invoices
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
-    <div>Dashboard</div>
+    <main>
+
+<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={numberOfInvoices} type="collected" /> 
+        <Card title="Pending" value={numberOfCustomers} type="pending" />
+        <Card title="Total Invoices" value={totalPaidInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={totalPendingInvoices}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
   )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,19 +11,12 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
